5556538523a6c7e7857df9822a26329c5fa47cfc
François Saint-Jacques <fsaintjacques@gmail.com>
2015-09-11 21:29:26 -0400

    adds set operations and header documentation to bloomfilters

--- src/libtwiddle/bloomfilter.c
+++ src/libtwiddle/bloomfilter.c
@@ -50,6 +50,18 @@ tw_bloomfilter_copy(const struct tw_bloomfilter *src,
   return dst;
 }
 
+struct tw_bloomfilter *
+tw_bloomfilter_clone(const struct tw_bloomfilter *bf)
+{
+  assert(bf);
+
+  struct tw_bloomfilter *new = tw_bloomfilter_new(bf->info.size, bf->info.k);
+  if (!new)
+    return NULL;
+
+  return tw_bloomfilter_copy(bf, new);
+}
+
 void
 tw_bloomfilter_set(struct tw_bloomfilter *bf,
                    size_t size, const char* buf)
@@ -87,4 +99,105 @@ tw_bloomfilter_test(const struct tw_bloomfilter *bf,
   }
 
   return true;
+}
+
+bool
+tw_bloomfilter_empty(const struct tw_bloomfilter *bf)
+{
+  assert(bf);
+  return tw_bitmap_empty(bf->bitmap);
+}
+
+bool
+tw_bloomfilter_full(const struct tw_bloomfilter *bf)
+{
+  assert(bf);
+  return tw_bitmap_full(bf->bitmap);
+}
+
+uint32_t
+tw_bloomfilter_count(const struct tw_bloomfilter *bf)
+{
+  assert(bf);
+  return tw_bitmap_count(bf->bitmap);
+}
+
+float
+tw_bloomfilter_density(const struct tw_bloomfilter *bf)
+{
+  assert(bf);
+  return tw_bitmap_density(bf->bitmap);
+}
+
+struct tw_bloomfilter *
+tw_bloomfilter_zero(struct tw_bloomfilter *bf)
+{
+  assert(bf);
+  return (tw_bitmap_zero(bf->bitmap)) ? bf : NULL;
+}
+
+struct tw_bloomfilter *
+tw_bloomfilter_fill(struct tw_bloomfilter *bf)
+{
+  assert(bf);
+  return (tw_bitmap_fill(bf->bitmap)) ? bf : NULL;
+}
+
+struct tw_bloomfilter *
+tw_bloomfilter_not(struct tw_bloomfilter *bf)
+{
+  assert(bf);
+  return (tw_bitmap_not(bf->bitmap))? bf : NULL;
+}
+
+
+bool
+tw_bloomfilter_equal(const struct tw_bloomfilter *a,
+                     const struct tw_bloomfilter *b)
+{
+  assert(a && b);
+
+  if (!tw_bloomfilter_info_equal(a->info, b->info))
+    return false;
+
+  return tw_bitmap_equal(a->bitmap, b->bitmap);
+}
+
+
+struct tw_bloomfilter *
+tw_bloomfilter_union(const struct tw_bloomfilter *src,
+                           struct tw_bloomfilter *dst)
+{
+  assert(src && dst);
+
+  if (!tw_bloomfilter_info_equal(src->info, dst->info))
+    return NULL;
+
+  return (tw_bitmap_union(src->bitmap, dst->bitmap)) ? dst : NULL;
+}
+
+
+struct tw_bloomfilter *
+tw_bloomfilter_intersection(const struct tw_bloomfilter *src,
+                                  struct tw_bloomfilter *dst)
+{
+  assert(src && dst);
+
+  if (!tw_bloomfilter_info_equal(src->info, dst->info))
+    return NULL;
+
+  return (tw_bitmap_intersection(src->bitmap, dst->bitmap)) ? dst : NULL;
+}
+
+
+struct tw_bloomfilter *
+tw_bloomfilter_xor(const struct tw_bloomfilter *src,
+                         struct tw_bloomfilter *dst)
+{
+  assert(src && dst);
+
+  if (!tw_bloomfilter_info_equal(src->info, dst->info))
+    return NULL;
+
+  return (tw_bitmap_xor(src->bitmap, dst->bitmap)) ? dst : NULL;
 }

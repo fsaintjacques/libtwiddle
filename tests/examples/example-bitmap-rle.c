d8d9a742dc8654e6de0157515b3a4c9187cfcd0a
François Saint-Jacques <fsaintjacques@gmail.com>
2015-09-24 10:24:30 -0400

    Updates bitmap_rle example documentation

--- tests/examples/example-bitmap-rle.c
+++ tests/examples/example-bitmap-rle.c
@@ -2,16 +2,24 @@
 #include <libtwiddle/bitmap_rle.h>
 
 int main(int argc, char* argv[]) {
-  /** let's allocate a bitmap containing 2 billions bits */
+  /** allocate a bitmap containing 2 billions bits */
   const uint32_t nbits = 1UL << 31;
   struct tw_bitmap_rle* bitmap = tw_bitmap_rle_new(nbits);
 
   assert(bitmap);
 
-  /** let's fill 1 billion bits */
+  /** fill 1 billion bits */
   const uint32_t start = 0UL;
   const uint32_t end = 1UL << 30;
   tw_bitmap_rle_set_range(bitmap, start, end);
+
+  /**
+   * bitmap_rle DOES NOT support setting bits in non sorted order, e.g.
+   * calling the following will raise an assert() call (or undefined behaviour
+   * if compiled with NDEBUG):
+   *
+   * tw_bitmap_rle_set(bitmap, start - 1);
+   */
 
   assert(tw_bitmap_rle_test(bitmap, start));
   assert(tw_bitmap_rle_test(bitmap, end));

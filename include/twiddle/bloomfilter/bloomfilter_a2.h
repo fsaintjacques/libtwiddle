54545a24f10c2de25e2cb07532e7b21c9a0c877b
François Saint-Jacques <fsaintjacques@gmail.com>
2015-11-23 14:09:25 +0000

    Initial implementation

---
+++ include/twiddle/bloomfilter/bloomfilter_a2.h
@@ -0,0 +1,87 @@
+#ifndef TWIDDLE_BLOOMFILTER_A2_H
+#define TWIDDLE_BLOOMFILTER_A2_H
+
+#include <twiddle/bloomfilter/bloomfilter.h>
+
+struct tw_bloomfilter_a2 {
+  float density;
+
+  struct tw_bloomfilter *active;
+  struct tw_bloomfilter *passive;
+};
+
+struct tw_bloomfilter_a2 *
+tw_bloomfilter_a2_new(uint64_t size, uint16_t k, float dentisy);
+
+
+struct tw_bloomfilter_a2 *
+tw_bloomfilter_a2_copy(const struct tw_bloomfilter_a2 *src,
+                       struct tw_bloomfilter_a2 *dst);
+
+
+struct tw_bloomfilter_a2 *
+tw_bloomfilter_a2_clone(const struct tw_bloomfilter_a2 *bf);
+
+
+void
+tw_bloomfilter_a2_free(struct tw_bloomfilter_a2 *bf);
+
+
+void
+tw_bloomfilter_a2_set(struct tw_bloomfilter_a2 *bf,
+                      size_t size, const char* buf);
+
+
+bool
+tw_bloomfilter_a2_test(const struct tw_bloomfilter_a2 *bf,
+                       size_t size, const char* buf);
+
+
+bool
+tw_bloomfilter_a2_empty(const struct tw_bloomfilter_a2 *bf);
+
+
+bool
+tw_bloomfilter_a2_full(const struct tw_bloomfilter_a2 *bf);
+
+
+uint64_t
+tw_bloomfilter_a2_count(const struct tw_bloomfilter_a2 *bf);
+
+
+float
+tw_bloomfilter_a2_density(const struct tw_bloomfilter_a2 *bf);
+
+
+struct tw_bloomfilter_a2 *
+tw_bloomfilter_a2_zero(struct tw_bloomfilter_a2 *bf);
+
+
+struct tw_bloomfilter_a2 *
+tw_bloomfilter_a2_fill(struct tw_bloomfilter_a2 *bf);
+
+
+struct tw_bloomfilter_a2 *
+tw_bloomfilter_a2_not(struct tw_bloomfilter_a2 *bf);
+
+
+bool
+tw_bloomfilter_a2_equal(const struct tw_bloomfilter_a2 *a,
+                        const struct tw_bloomfilter_a2 *b);
+
+
+struct tw_bloomfilter_a2 *
+tw_bloomfilter_a2_union(const struct tw_bloomfilter_a2 *src,
+                              struct tw_bloomfilter_a2 *dst);
+
+
+struct tw_bloomfilter_a2 *
+tw_bloomfilter_a2_intersection(const struct tw_bloomfilter_a2 *src,
+                                     struct tw_bloomfilter_a2 *dst);
+
+
+struct tw_bloomfilter_a2 *
+tw_bloomfilter_a2_xor(const struct tw_bloomfilter_a2 *src,
+                            struct tw_bloomfilter_a2 *dst);
+
+#endif /* TWIDDLE_BLOOMFILTER_A2_H */
---
+++ src/twiddle/bloomfilter/bloomfilter_a2.c
@@ -0,0 +1,228 @@
+#include <assert.h>
+
+#include <twiddle/bloomfilter/bloomfilter_a2.h>
+
+struct tw_bloomfilter_a2 *
+tw_bloomfilter_a2_new(uint64_t size, uint16_t k, float density)
+{
+  assert(size > 0 && size <= TW_BITMAP_MAX_BITS && k > 0 &&
+         0 < density && density <= 1);
+
+  struct tw_bloomfilter_a2 *bf = calloc(1, sizeof(struct tw_bloomfilter_a2));
+
+  struct tw_bloomfilter *active = tw_bloomfilter_new(size, k);
+  if (!active) {
+    free(bf);
+    return NULL;
+  }
+
+  struct tw_bloomfilter *passive = tw_bloomfilter_new(size, k);
+  if (!passive) {
+    free(bf);
+    free(active);
+    return NULL;
+  }
+
+  bf->density = density;
+  bf->active  = active;
+  bf->passive = passive;
+
+  return bf;
+}
+
+
+struct tw_bloomfilter_a2 *
+tw_bloomfilter_a2_copy(const struct tw_bloomfilter_a2 *src,
+                       struct tw_bloomfilter_a2 *dst)
+{
+  assert(src && dst);
+
+  if (!tw_bloomfilter_copy(src->active,  dst->active) ||
+      !tw_bloomfilter_copy(src->passive, dst->passive)) {
+    return NULL;
+  }
+
+  return dst;
+}
+
+
+struct tw_bloomfilter_a2 *
+tw_bloomfilter_a2_clone(const struct tw_bloomfilter_a2 *bf)
+{
+  struct tw_bloomfilter_a2 *new =
+    tw_bloomfilter_a2_new(bf->active->bitmap->info.size,
+                          bf->active->info.k,
+                          bf->density);
+  if (!new) {
+    return NULL;
+  }
+
+  return tw_bloomfilter_a2_copy(bf, new);
+}
+
+
+void
+tw_bloomfilter_a2_free(struct tw_bloomfilter_a2 *bf)
+{
+  assert(bf);
+
+  tw_bloomfilter_free(bf->active);
+  tw_bloomfilter_free(bf->passive);
+  free(bf);
+}
+
+
+void
+tw_bloomfilter_a2_set(struct tw_bloomfilter_a2 *bf,
+                      size_t size, const char* buf)
+{
+  assert(bf && buf && size > 0);
+
+  /**
+   * We assume rotation is a rare event, thus the tw_unlikely hint.
+   */
+  if (tw_unlikely(tw_bloomfilter_density(bf->active) >= bf->density)) {
+    struct tw_bloomfilter *tmp = bf->passive;
+    bf->passive = bf->active;
+    bf->active = tmp;
+    tw_bloomfilter_zero(tmp);
+  }
+
+  tw_bloomfilter_set(bf->active, size, buf);
+}
+
+
+bool
+tw_bloomfilter_a2_test(const struct tw_bloomfilter_a2 *bf,
+                       size_t size, const char* buf)
+{
+  assert(bf && buf && size > 0);
+
+  return tw_bloomfilter_test(bf->active,  size, buf) ||
+         tw_bloomfilter_test(bf->passive, size, buf);
+}
+
+
+bool
+tw_bloomfilter_a2_empty(const struct tw_bloomfilter_a2 *bf)
+{
+  assert(bf);
+
+  return tw_bloomfilter_empty(bf->active) &&
+         tw_bloomfilter_empty(bf->passive);
+}
+
+
+bool
+tw_bloomfilter_a2_full(const struct tw_bloomfilter_a2 *bf)
+{
+  assert(bf);
+
+  return tw_bloomfilter_full(bf->active) &&
+         tw_bloomfilter_full(bf->passive);
+}
+
+
+uint64_t
+tw_bloomfilter_a2_count(const struct tw_bloomfilter_a2 *bf)
+{
+  assert(bf);
+
+  return tw_bloomfilter_count(bf->active) +
+         tw_bloomfilter_count(bf->passive);
+}
+
+float
+tw_bloomfilter_a2_density(const struct tw_bloomfilter_a2 *bf)
+{
+  assert(bf);
+
+  return (tw_bloomfilter_density(bf->active) +
+          tw_bloomfilter_density(bf->passive)) / 2.0;
+}
+
+
+struct tw_bloomfilter_a2 *
+tw_bloomfilter_a2_zero(struct tw_bloomfilter_a2 *bf)
+{
+  assert(bf);
+
+  return (tw_bloomfilter_zero(bf->active) &&
+          tw_bloomfilter_zero(bf->passive)) ? bf : NULL;
+}
+
+struct tw_bloomfilter_a2 *
+tw_bloomfilter_a2_fill(struct tw_bloomfilter_a2 *bf)
+{
+  assert(bf);
+
+  return (tw_bloomfilter_fill(bf->active) &&
+          tw_bloomfilter_fill(bf->passive)) ? bf : NULL;
+}
+
+
+struct tw_bloomfilter_a2 *
+tw_bloomfilter_a2_not(struct tw_bloomfilter_a2 *bf)
+{
+  assert(bf);
+
+  return (tw_bloomfilter_not(bf->active) &&
+          tw_bloomfilter_not(bf->passive)) ? bf : NULL;
+}
+
+
+bool
+tw_bloomfilter_a2_equal(const struct tw_bloomfilter_a2 *a,
+                        const struct tw_bloomfilter_a2 *b)
+{
+  assert(a && b);
+
+  return (a->density == b->density &&
+          tw_bloomfilter_equal(a->active,  b->active) &&
+          tw_bloomfilter_equal(a->passive, b->passive));
+}
+
+
+struct tw_bloomfilter_a2 *
+tw_bloomfilter_a2_union(const struct tw_bloomfilter_a2 *src,
+                              struct tw_bloomfilter_a2 *dst)
+{
+  assert(src && dst);
+
+  if (src->density != dst->density) {
+    return NULL;
+  }
+
+  return (tw_bloomfilter_union(src->active,  dst->active) &&
+          tw_bloomfilter_union(src->passive, dst->passive)) ? dst : NULL;
+}
+
+
+struct tw_bloomfilter_a2 *
+tw_bloomfilter_a2_intersection(const struct tw_bloomfilter_a2 *src,
+                                     struct tw_bloomfilter_a2 *dst)
+{
+  assert(src && dst);
+
+  if (src->density != dst->density) {
+    return NULL;
+  }
+
+  return (tw_bloomfilter_intersection(src->active,  dst->active) &&
+          tw_bloomfilter_intersection(src->passive, dst->passive)) ? dst : NULL;
+}
+
+
+struct tw_bloomfilter_a2 *
+tw_bloomfilter_a2_xor(const struct tw_bloomfilter_a2 *src,
+                            struct tw_bloomfilter_a2 *dst)
+{
+  assert(src && dst);
+
+  if (src->density != dst->density) {
+    return NULL;
+  }
+
+  return (tw_bloomfilter_xor(src->active,  dst->active) &&
+          tw_bloomfilter_xor(src->passive, dst->passive)) ? dst : NULL;
+}

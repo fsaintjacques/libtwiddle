ab7af295bed3066f3c1324237c7bcc110cd75bb4
François Saint-Jacques <fsaintjacques@gmail.com>
2015-12-06 15:46:04 -0500

    Adds bloomfilter-a2 python bindings and tests

--- python/twiddle/__init__.py
+++ python/twiddle/__init__.py
@@ -1,6 +1,7 @@
-from bitmap      import Bitmap
-from bitmap_rle  import BitmapRLE
-from bloomfilter import BloomFilter
-from hyperloglog import HyperLogLog
+from bitmap         import Bitmap
+from bitmap_rle     import BitmapRLE
+from bloomfilter    import BloomFilter
+from bloomfilter_a2 import BloomFilterA2
+from hyperloglog    import HyperLogLog
 
-__all__ = ['Bitmap', 'BitmapRLE', 'BloomFilter', 'HyperLogLog']
+__all__ = ['Bitmap', 'BitmapRLE', 'BloomFilter', 'BloomFilterA2', 'HyperLogLog']

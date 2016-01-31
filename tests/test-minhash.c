2bc5715169304c074af81fbcad46ff9c94747401
François Saint-Jacques <fsaintjacques@gmail.com>
2016-01-30 22:37:30 -0500

    Fix unstable minhash test with travis

--- tests/test-minhash.c
+++ tests/test-minhash.c
@@ -150,6 +150,8 @@ int run_tests()
   tcase_add_test(tc, test_minhash_basic);
   tcase_add_test(tc, test_minhash_copy_and_clone);
   tcase_add_test(tc, test_minhash_merge);
+  /* added for travis slowness of clang */
+  tcase_set_timeout(tc, 15);
   suite_add_tcase(s, tc);
   srunner_run_all(runner, CK_NORMAL);
   number_failed = srunner_ntests_failed(runner);

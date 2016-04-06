b2489f1039024736ecaa26c41bdc125d44247f15
François Saint-Jacques <fsaintjacques@gmail.com>
2016-04-05 20:56:27 -0400

    Fixes twiddle.h guards

--- include/twiddle.h
+++ include/twiddle.h
@@ -1,6 +1,14 @@
-#ifndef TWIDDLE_BITMAP_H
-#define TWIDDLE_BITMAP_H
+#ifndef TWIDDLE_H
+#define TWIDDLE_H
 
 #include <twiddle/bitmap/bitmap.h>
+#include <twiddle/bitmap/bitmap_rle.h>
 
-#endif /* TWIDDLE_BITMAP_H */
+#include <twiddle/bloomfilter/bloomfilter.h>
+#include <twiddle/bloomfilter/bloomfilter_a2.h>
+
+#include <twiddle/hash/minhash.h>
+
+#include <twiddle/hyperloglog/hyperloglog.h>
+
+#endif /* TWIDDLE_H */

720a3d82403dc893b65859422b71ff1efbd26e0b
François Saint-Jacques <fsaintjacques@gmail.com>
2016-08-16 10:34:27 -0400

    Sanitize macros

--- src/twiddle/macrology.h
+++ src/twiddle/macrology.h
@@ -14,15 +14,15 @@
 #define TW_DIV_ROUND_UP(n, d) (((n) + (d)-1) / (d))
 
 #define TW_ALLOC_TO_CACHELINE(size)                                            \
-  ((TW_DIV_ROUND_UP(size, TW_CACHELINE) * TW_CACHELINE))
+  ((TW_DIV_ROUND_UP((size), TW_CACHELINE) * TW_CACHELINE))
 
 #define TW_ARRAY_SIZE(array) (sizeof(array) / sizeof(array[0]))
 
 #define TW_BITOP_ADDR(x) "+m"(*(volatile long *)(x))
 
 #define TW_BITS_IN_WORD 8
-#define TW_BIT_POS(x) (1 << (x % TW_BITS_IN_WORD))
-#define TW_BYTE_POS(x) (x / TW_BITS_IN_WORD)
+#define TW_BIT_POS(x) (1 << ((x) % TW_BITS_IN_WORD))
+#define TW_BYTE_POS(x) ((x) / TW_BITS_IN_WORD)
 
 #define tw_likely(x) __builtin_expect((x), 1)
 #define tw_unlikely(x) __builtin_expect((x), 0)
@@ -31,7 +31,7 @@
 #define tw_min(a, b) (((a) < (b)) ? (a) : (b))
 #define tw_max(a, b) (((a) < (b)) ? (b) : (a))
 
-#define tw_almost_equal(a, b) (fabs(a - b) < FLT_EPSILON)
+#define tw_almost_equal(a, b) (fabs((a) - (b)) < FLT_EPSILON)
 
 /* OSX doesn't provide `aligned_alloc` like C11 mandates (we're in 2016). */
 #if defined(__APPLE__)
@@ -50,12 +50,12 @@ static inline void *__aligned_alloc(size_t align, size_t size)
 #endif
 
 #define tw_simd_equal(a, b, simd_cmpeq, simd_maskmove, mask)                   \
-  ((int)mask == simd_maskmove(simd_cmpeq(a, b)))
+  ((int)mask == simd_maskmove(simd_cmpeq((a), (b))))
 
 #define tw_mm256_equal(a, b)                                                   \
-  tw_simd_equal(a, b, _mm256_cmpeq_epi8, _mm256_movemask_epi8, 0xFFFFFFFF)
+  tw_simd_equal((a), (b), _mm256_cmpeq_epi8, _mm256_movemask_epi8, 0xFFFFFFFF)
 
 #define tw_mm_equal(a, b)                                                      \
-  tw_simd_equal(a, b, _mm_cmpeq_epi8, _mm_movemask_epi8, 0xFFFF)
+  tw_simd_equal((a), (b), _mm_cmpeq_epi8, _mm_movemask_epi8, 0xFFFF)
 
 #endif /* TWIDDLE_INTERNAL_UTILS_H */

ad4dcaa6d24785f514cbb98c18e8ead2bffbff33
François Saint-Jacques <fsaintjacques@gmail.com>
2016-08-16 09:40:37 -0400

    Improve MinHash documentation

--- include/twiddle/hash/minhash.h
+++ include/twiddle/hash/minhash.h
@@ -7,11 +7,39 @@
 /**
  * minhash data structure
  *
- * This implementation forces use of 32bits buckets. It also uses max instead
+ * (source https://en.wikipedia.org/wiki/MinHash)
+ *
+ * MinHash (or the min-wise independent permutations locality sensitive hashing
+ * scheme) is a technique for quickly estimating how similar two sets are. The
+ * term MinHash was coined by Andrei Broder (1997), see [1]. This is also known
+ * as the `k-mins` sketch.
+ *
+ * The simplest version of the MinHash scheme uses `k` different hash functions,
+ * where `k` is a fixed integer parameter, and represents each set `S` by the
+ * `k` values of `h_min(S)` for these `k` functions.
+ *
+ * To estimate `Jaccard(A, B)` using this version of the scheme, let `y` be the
+ * number of hash functions for which `h_min(A) = h_min(B)`, and use `y/k` as
+ * the estimate. This estimate is the average of `k` different 0-1 random
+ * variables, each of which is one when `h_min(A) = h_min(B)` and zero
+ * otherwise, and each of which is an unbiased estimator of `Jaccard(A, B)`.
+ * Therefore, their average is also an unbiased estimator, and by standard
+ * Chernoff bounds for sums of 0-1 random variables, its expected error is
+ * `O(1/√k)`.
+ *
+ * Therefore, for any constant `ε > 0` there is a constant `k = O(1/ε^2)` such
+ * that the expected error of the estimate is at most `ε`. For example, 400
+ * hashes would be required to estimate `Jaccard(A, B)` with an expected error
+ * less than or equal to .05.
+ *
+ * This implementation uses 32bits registers. It also uses max instead
  * of min, since it's homomorphic.
+ *
+ * [1] Broder, Andrei Z. "On the resemblance and containment of documents."
+ * Compression and Complexity of Sequences 1997. Proceedings. IEEE, 1997.
  */
 struct tw_minhash {
-  /** number of registers */
+  /** number of registers, also the number of hash functions */
   uint32_t n_registers;
   /** registers holding computed values */
   uint32_t *registers;
